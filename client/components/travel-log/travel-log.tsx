a90e96f9889287190ef2a1cb00149cdd88d929b6
shvam0000 <shvam0000@gmail.com>
2022-04-08 11:10:42 +0530

    :sparkles: add travel log cards with dummy data

--- client/components/travel-log/travel-log.tsx
+++ client/components/travel-log/travel-log.tsx
@@ -1,10 +1,42 @@
-import { MainHeader, AddLog } from './';
+import { MainHeader, AddLog, Card } from './';
 
 const TravelLogComponent = () => {
   return (
     <>
       <MainHeader />
       <AddLog />
+      <div className='flex flex-wrap mx-10 my-10'>
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+        <Card
+          name='Shivam Shekhar'
+          title='Pondi'
+          experience='Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sunt!'
+        />
+      </div>
     </>
   );
 };
