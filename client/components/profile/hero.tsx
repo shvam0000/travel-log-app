5899533aba41543b920c0a892e1ff1c6a0092f1c
shvam0000 <shvam0000@gmail.com>
2022-05-07 01:32:00 +0530

    :sparkles: Profile Page

---
+++ client/components/profile/hero.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Subnavbar } from '../shared';
+
+const ProfileHero = () => {
+  const [userName, setUserName] = useState<string | any>('');
+  const [email, setEmail] = useState<string | any>('');
+
+  useEffect(() => {
+    setUserName(localStorage.getItem('username'));
+    setEmail(localStorage.getItem('email'));
+  }, []);
+
+  return (
+    <div>
+      <div className="my-5 bg-blue-400 w-60 py-4 px-2 mx-auto rounded-xl text-white font-bold text-xl flex justify-center items-center">
+        Profile
+      </div>
+      <div className="py-10">
+        <Subnavbar />
+      </div>
+      <div className="text-xl font-bold flex justify-center items-center">
+        Username - {userName}
+      </div>
+      <div className="text-xl font-bold flex justify-center items-center">
+        Email - {email}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHero;
